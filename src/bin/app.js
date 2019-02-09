09ce9af266d72428cd984afd21605b2ec7d55713
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2019-02-09 16:54:47 +0000

    Launch server with cluster

--- src/bin/app.js
+++ src/bin/app.js
@@ -1,14 +1,34 @@
+const cluster = require('cluster')
 const app = require('../app')
 const logger = require('../utils/logger')
 const eventStream = require('../controllers/event-stream')
 
-function getServerPort () {
-  return 3000 + (parseInt(process.env.NODE_APP_INSTANCE, 10) || 0)
+function getNumProcesses () {
+  return parseInt(process.env.THEMIS_QUALS_NUM_PROCESSES || '2', 10)
 }
 
-let hostname = process.env.THEMIS_QUALS_HOST || '127.0.0.1'
+function getServerPort () {
+  return parseInt(process.env.THEMIS_QUALS_PORT || '8000', 10)
+}
 
-var server = app.listen(getServerPort(), hostname, function () {
-  logger.info(`Server listening on ${server.address().address}:${server.address().port}`)
-  eventStream.run()
-})
+function getServerHost () {
+  return process.env.THEMIS_QUALS_HOST || '127.0.0.1'
+}
+
+if (cluster.isMaster) {
+  logger.info(`Master ${process.pid} is running`)
+
+  for (let i=0; i<getNumProcesses(); i++) {
+    cluster.fork()
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    logger.info(`Worker ${worker.process.pid} died`)
+  })
+} else {
+  let server = app.listen(getServerPort(), getServerHost(), function () {
+    logger.info(`Worker ${process.pid}, server listening on ${server.address().address}:${server.address().port}`)
+    eventStream.run()
+  })
+  logger.info(`Worker ${process.pid} started`)
+}
