96a8cccae7d3606e6cbfc8c00f5d06744b23a7a3
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2019-02-18 17:14:50 +0000

    Rewrite queue launch script

--- src/bin/queue.js
+++ src/bin/queue.js
@@ -1,4 +1,24 @@
-require('../queue')
+const cluster = require('cluster');
+const logger = require('../utils/logger')
 const eventStream = require('../controllers/event-stream')
 
-eventStream.run()
+function getNumProcesses () {
+  return parseInt(process.env.THEMIS_QUALS_NUM_PROCESSES || '2', 10)
+}
+
+if (cluster.isMaster) {
+  logger.info(`Master ${process.pid} is running`)
+
+  // Fork workers.
+  for (let i=0; i<getNumProcesses(); i++) {
+    cluster.fork()
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    logger.info(`Worker ${worker.process.pid} died`)
+  })
+} else {
+  require('../queue')
+  eventStream.run()
+  logger.info(`Worker ${process.pid} started`)
+}
