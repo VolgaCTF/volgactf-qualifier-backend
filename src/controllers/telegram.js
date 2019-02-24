edcf2c462be14d3cc8ef812d7afc7816d3bb03ef
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2019-02-24 11:10:10 +0000

    Support Telegram with proxy

--- src/controllers/telegram.js
+++ src/controllers/telegram.js
@@ -1,15 +1,26 @@
-// const logger = require('../utils/logger')
 const request = require('request')
+const Agent = require('socks5-https-client/lib/Agent')
 
 class TelegramController {
   constructor () {
     this.botAccessToken = process.env.TELEGRAM_BOT_ACCESS_TOKEN
     this.chatId = process.env.TELEGRAM_CHAT_ID
+    this.socks5 = process.env.hasOwnProperty('TELEGRAM_SOCKS5_HOST') && process.env.TELEGRAM_SOCKS5_HOST !== '' && process.env.hasOwnProperty('TELEGRAM_SOCKS5_PORT') && process.env.TELEGRAM_SOCKS5_PORT !== ''
+    if (this.socks5) {
+      this.socks5Options = {
+        socksHost: process.env.TELEGRAM_SOCKS5_HOST,
+        socksPort: parseInt(process.env.TELEGRAM_SOCKS5_PORT, 10)
+      }
+      if (process.env.hasOwnProperty('TELEGRAM_SOCKS5_USERNAME') && process.env.TELEGRAM_SOCKS5_USERNAME !== '' && process.env.hasOwnProperty('TELEGRAM_SOCKS5_PASSWORD') && process.env.TELEGRAM_SOCKS5_PASSWORD !== '') {
+        this.socks5Options.socksUsername = process.env.TELEGRAM_SOCKS5_USERNAME
+        this.socks5Options.socksPassword = process.env.TELEGRAM_SOCKS5_PASSWORD
+      }
+    }
   }
 
   post (description) {
     return new Promise((resolve, reject) => {
-      request({
+      const params = {
         method: 'GET',
         url: `https://api.telegram.org/bot${this.botAccessToken}/sendMessage`,
         qs: {
@@ -17,7 +28,12 @@ class TelegramController {
           text: description,
           parse_mode: 'Markdown'
         }
-      }, (err, response, body) => {
+      }
+      if (this.socks5) {
+        params.agentClass = Agent
+        params.agentOptions = this.socks5Options
+      }
+      request(params, function (err, response, body) {
         if (err) {
           reject(err)
         } else {
