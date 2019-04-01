af7dbc27deb9e7b22fb186312c12d39d48cbbe4d
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2019-04-01 05:59:14 +0000

    CSV teams

--- src/routes/third-party.js
+++ src/routes/third-party.js
@@ -4,9 +4,14 @@ const router = express.Router()
 const logger = require('../utils/logger')
 const { InternalError } = require('../utils/errors')
 const TeamController = require('../controllers/team')
+const CountryController = require('../controllers/country')
 const TeamRanking = require('../models/team-ranking')
+
 const _ = require('underscore')
 const jsesc = require('jsesc')
+const csv = require('csv-express')
+
+const { needsToBeAuthorizedSupervisor } = require('../middleware/session')
 
 function rank (a, b) {
   if (a.position < b.position) {
@@ -57,6 +62,51 @@ router.get('/ctftime', function (request, response, next) {
         })
     }
   }, true)
+})
+
+router.get('/teams', needsToBeAuthorizedSupervisor, function (request, response, next) {
+  CountryController.index(function (err, countries) {
+    if (err) {
+      next(new InternalError(), null)
+    } else {
+      TeamController.index(function (err, teams) {
+        if (err) {
+          logger.error(err)
+          next(new InternalError(), null)
+        } else {
+          TeamRanking
+            .query()
+            .then(function (teamRankings) {
+              const entries = _.map(teamRankings, function (teamRanking) {
+                const team = _.findWhere(teams, { id: teamRanking.teamId })
+                return {
+                  team: team,
+                  position: teamRanking.position,
+                  score: teamRanking.score
+                }
+              })
+              entries.sort(rank)
+              response.csv(_.map(entries, function (entry, ndx) {
+                const country = _.findWhere(countries, { id: entry.team.countryId })
+                return {
+                  position: ndx + 1,
+                  team: entry.team.name,
+                  email: entry.team.email,
+                  country: country.name,
+                  locality: entry.team.locality,
+                  institution: entry.team.institution,
+                  score: entry.score
+                }
+              }), true)
+            })
+            .catch(function (err) {
+              logger.error(err)
+              next(new InternalError(), null)
+            })
+        }
+      }, true)
+    }
+  })
 })
 
 module.exports = router
